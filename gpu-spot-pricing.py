5a94c9e353ea3925d47707a6aba248350186151c
Justin Su <injustsu@gmail.com>
2026-03-02 01:52:47 -0500

    Show progress when querying spot pricing

--- gpu-spot-pricing.py
+++ gpu-spot-pricing.py
@@ -13,6 +13,8 @@ import anyio
 from aiobotocore.session import get_session
 from anyio import create_memory_object_stream, create_task_group
 from attrs import field, frozen
+from rich.console import Console
+from rich.progress import Progress, SpinnerColumn, TextColumn
 
 try:
     from rich import get_console
@@ -67,7 +69,9 @@ class Pricing:
                 raise ValueError(self.instance_type)
 
 
-async def query_region(region: str, output: MemoryObjectSendStream[Pricing]) -> None:
+async def query_region(region: str, progress: Progress, output: MemoryObjectSendStream[Pricing]) -> None:
+    task = progress.add_task(region, total=None)
+
     async with aws.create_client("ec2", region) as ec2, output:  # pyright: ignore[reportUnknownMemberType]
         paginator = ec2.get_paginator("describe_spot_price_history")
         async for page in paginator.paginate(
@@ -78,27 +82,33 @@ async def query_region(region: str, output: MemoryObjectSendStream[Pricing]) -> 
             for pricing in map(Pricing.from_dict, page["SpotPriceHistory"]):
                 await output.send(pricing)
 
+        progress.update(task, visible=False, refresh=True)
+
 
 async def main() -> None:
     REGIONS = await aws.get_available_regions("ec2")
 
     results: list[Pricing] = []
     send_stream, receive_stream = create_memory_object_stream[Pricing]()
-    async with create_task_group() as tg, receive_stream:
-        for region in REGIONS:
-            tg.start_soon(query_region, region, send_stream.clone())
-        await send_stream.aclose()
 
-        async for pricing in receive_stream:
-            max_price = pricing.gpu_count * MAX_GPU_HOUR_PRICE
-            match pricing.instance_family:
-                case InstanceFamily.G5:
-                    max_price /= G6E_G5_PERF_RATIO
-                case InstanceFamily.G6E:
-                    pass
+    with Progress(
+        SpinnerColumn(), TextColumn("[progress.description]{task.description}"), console=Console(stderr=True)
+    ) as progress:
+        async with create_task_group() as tg, receive_stream:
+            for region in REGIONS:
+                tg.start_soon(query_region, region, progress, send_stream.clone())
+            await send_stream.aclose()
 
-            if pricing.spot_price <= max_price:
-                results.append(pricing)
+            async for pricing in receive_stream:
+                max_price = pricing.gpu_count * MAX_GPU_HOUR_PRICE
+                match pricing.instance_family:
+                    case InstanceFamily.G5:
+                        max_price /= G6E_G5_PERF_RATIO
+                    case InstanceFamily.G6E:
+                        pass
+
+                if pricing.spot_price <= max_price:
+                    results.append(pricing)
 
     results.sort()
     for pricing in results:
